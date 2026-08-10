4c278973bca4f4168cc7cdb227ce84f555c519b4
greendwarf1-sketch <greendwarf1@gmail.com>
2026-08-10 13:21:33 +0200

    Add files via upload

--- exportclient.js
+++ exportclient.js
@@ -2,7 +2,7 @@
 /**********************************************************************
  * Copyright (C) 2026 Mr. Green & MCS
  * 
- * exportclient.js - DEBUG SKENER ZA OTKRIVANJE STRUKTURE BAZE
+ * exportclient.js - Backend Full Export + Smart DB Error Handler
  **********************************************************************/
 
 module.exports.exportclient = function (parent) {
@@ -84,7 +84,7 @@ module.exports.exportclient = function (parent) {
                             '<h4 style="margin-top:0; color:#333; font-weight:bold;">mTicket Client Export</h4>' +
                             '<p style="font-size:13px; color:#666; margin-bottom:15px;">Preuzmite POTPUNU hardversku specifikaciju i popis softvera izravno iz baze podataka.</p>' +
                             '<button id="btn-export-csv" class="btn btn-secondary btn-sm" style="margin-right:10px;">Preuzmi CSV</button>' +
-                            '<button id="btn-export-ticket" class="btn btn-danger btn-sm">Pokreni Debug Skener (TXT)</button>' +
+                            '<button id="btn-export-ticket" class="btn btn-primary btn-sm">Preuzmi TXT (Ticketing)</button>' +
                             '</div>';
         p19.appendChild(myPanel);
 
@@ -103,7 +103,7 @@ module.exports.exportclient = function (parent) {
     };
 
     // ====================================================================
-    // BACK-END: DEBUG SKENER (Vadi sve iz DB i RAM-a)
+    // BACK-END: PAMETNI HTTP HANDLER
     // ====================================================================
     obj.handleAdminReq = function(req, res, user) {
         
@@ -115,72 +115,110 @@ module.exports.exportclient = function (parent) {
             var sysid = nodeid.replace(/^node\/\//, 'si//');
             var swid  = nodeid.replace(/^node\/\//, 'sw//');
 
-            // Vadimo glavni node, sysinfo i software dokument iz baze
-            obj.meshServer.db.Get([nodeid, sysid, swid], function (err, docs) {
-                var node = null, sysinfo = null, software = null;
+            // Sigurna funkcija za čitanje baze (pokušava veliko i malo slovo)
+            var safeDbGet = function(id, callback) {
+                if (typeof obj.meshServer.db.Get === 'function') {
+                    obj.meshServer.db.Get(id, callback);
+                } else if (typeof obj.meshServer.db.get === 'function') {
+                    obj.meshServer.db.get(id, callback);
+                } else {
+                    callback("GREŠKA: Nije pronađena db.Get funkcija u MeshCentral jezgri!", null);
+                }
+            };
+
+            // 1. Čitanje glavnog Node dokumenta
+            safeDbGet(nodeid, function (err, nodes) {
                 
-                if (docs && docs.length > 0) {
-                    for (var i = 0; i < docs.length; i++) {
-                        if (docs[i]._id === nodeid) node = docs[i];
-                        else if (docs[i]._id === sysid) sysinfo = docs[i];
-                        else if (docs[i]._id === swid) software = docs[i];
-                    }
+                var node = null;
+                // Fleksibilna provjera (ako je MongoDB vraća Array, ako je NeDB može vratiti direktno objekt)
+                if (Array.isArray(nodes) && nodes.length > 0) node = nodes[0];
+                else if (nodes && !Array.isArray(nodes) && nodes._id) node = nodes;
+
+                // AKO PADNE, ISPISUJEMO DETALJE U BROWSER!
+                if (!node) {
+                    var debugHtml = "<h3>Greška baze podataka!</h3>" +
+                                    "<p><b>Traženi ID:</b> " + nodeid + "</p>" +
+                                    "<p><b>DB Error Varijabla:</b> " + String(err) + "</p>" +
+                                    "<p><b>DB Nodes Odgovor:</b> " + JSON.stringify(nodes) + "</p>";
+                    return res.send(debugHtml);
                 }
 
-                if (!node) return res.status(404).send('Računalo nije pronađeno u bazi.');
-                var safeName = (node.name || 'racunalo').replace(/[^a-z0-9]/gi, '_').toLowerCase();
+                // 2. Čitanje Hardvera
+                safeDbGet(sysid, function (err, sysnodes) {
+                    var sysinfo = null;
+                    if (Array.isArray(sysnodes) && sysnodes.length > 0) sysinfo = sysnodes[0];
+                    else if (sysnodes && !Array.isArray(sysnodes) && sysnodes._id) sysinfo = sysnodes;
 
-                // --------------------------------------------------------
-                // CSV Generiranje (Ostaje privremeno jednostavno)
-                // --------------------------------------------------------
-                if (req.query.download === 'csv') {
-                    res.setHeader('Content-disposition', 'attachment; filename=' + safeName + '_export.csv');
-                    res.setHeader('Content-type', 'text/csv; charset=utf-8');
-                    res.send("Kategorija,Svojstvo,Vrijednost\nOsnovno,Ime," + node.name + "\nOsnovno,ID," + node._id);
-                } 
-                // --------------------------------------------------------
-                // TXT Generiranje -> PRETVORENO U DEBUG SKENER
-                // --------------------------------------------------------
-                else if (req.query.download === 'ticket') {
-                    var txt = "=== MESH CENTRAL RAW DEBUG DUMP ===\n";
-                    txt += "Vrijeme: " + new Date().toLocaleString() + "\n";
-                    txt += "NODE ID: " + nodeid + "\n\n";
+                    // 3. Čitanje Softvera
+                    safeDbGet(swid, function (err, swnodes) {
+                        var software = null;
+                        if (Array.isArray(swnodes) && swnodes.length > 0) software = swnodes[0];
+                        else if (swnodes && !Array.isArray(swnodes) && swnodes._id) software = swnodes;
 
-                    txt += "--- 1. PODACI IZ GLAVNE BAZE (Node Document) ---\n";
-                    txt += JSON.stringify(node, null, 2) + "\n\n";
-                    
-                    txt += "--- 2. PODACI IZ ODVOJENIH DOKUMENATA (SysInfo / Software) ---\n";
-                    txt += "SysInfo: " + (sysinfo ? "PRONAĐEN\n" + JSON.stringify(sysinfo, null, 2) : "NIJE PRONAĐEN u bazi pod ID: " + sysid) + "\n\n";
-                    txt += "Software: " + (software ? "PRONAĐEN\n" + JSON.stringify(software, null, 2) : "NIJE PRONAĐEN u bazi pod ID: " + swid) + "\n\n";
+                        // Pripajanje podataka
+                        var hw = sysinfo ? (sysinfo.data || sysinfo.hwinfo || sysinfo) : node.hwinfo;
+                        var sw = software ? (software.data || software.apps || software.software || software) : node.software;
+                        
+                        var safeName = (node.name || 'racunalo').replace(/[^a-z0-9]/gi, '_').toLowerCase();
 
-                    txt += "--- 3. PODACI IZ RAM MEMORIJE (Trenutno aktivni agent) ---\n";
-                    
-                    // Pokušavamo locirati agenta u webserver memoriji
-                    var agent = null;
-                    if (obj.meshServer.webserver.wsagents && obj.meshServer.webserver.wsagents[nodeid]) {
-                        agent = obj.meshServer.webserver.wsagents[nodeid];
-                    }
+                        // --- CSV DOWNLOAD ---
+                        if (req.query.download === 'csv') {
+                            var csv = "Kategorija,Svojstvo,Vrijednost\n";
+                            csv += `Osnovno,Ime,${node.name || 'Nepoznato'}\n`;
+                            csv += `Osnovno,Opis,${node.desc || ''}\n`;
+                            csv += `Osnovno,OS,${node.osdesc || node.mtype || ''}\n`;
+                            
+                            if (sw) {
+                                var swList = sw.apps || sw; 
+                                if (Array.isArray(swList)) {
+                                    swList.forEach(function(app) {
+                                        var appName = (app.name || app.N || 'Nepoznato').replace(/,/g, ' '); 
+                                        var appVer = (app.version || app.V || '').replace(/,/g, ' ');
+                                        csv += `Softver,${appName},${appVer}\n`;
+                                    });
+                                } else {
+                                    csv += "Softver,Napomena,Pronađen nestandardni format softvera\n";
+                                }
+                            } else {
+                                csv += "Softver,Napomena,Agent još nije sinkronizirao softver u bazu\n";
+                            }
 
-                    if (agent) {
-                        txt += "Agent je ONLINE. Filtrirani RAM podaci:\n";
-                        var ramData = {
-                            hwinfo: agent.hwinfo || 'Nema',
-                            sysinfo: agent.sysinfo || 'Nema',
-                            machineinfo: agent.machineinfo || 'Nema',
-                            osdesc: agent.osdesc || 'Nema',
-                            software: agent.software || 'Nema'
-                        };
-                        txt += JSON.stringify(ramData, null, 2) + "\n";
-                    } else {
-                        txt += "Agent trenutno NIJE u RAM memoriji (Možda je uređaj offline ili wsagents nije dostupan).\n";
-                    }
+                            res.setHeader('Content-disposition', 'attachment; filename=' + safeName + '_export.csv');
+                            res.setHeader('Content-type', 'text/csv; charset=utf-8');
+                            res.send(csv);
+                        } 
+                        // --- TXT DOWNLOAD ---
+                        else if (req.query.download === 'ticket') {
+                            var txt = "=== MESH CENTRAL TICKET EXPORT ===\n";
+                            txt += "MC_NODE_ID: " + node._id + "\n";
+                            txt += "HOSTNAME: " + (node.name || 'N/A') + "\n";
+                            txt += "OS_TYPE: " + (node.osdesc || node.mtype || 'N/A') + "\n";
+                            txt += "DESCRIPTION: " + (node.desc || 'N/A') + "\n";
+                            
+                            if (node.host) txt += "LAST_IP: " + node.host + "\n";
+                            
+                            txt += "\n--- HARDWARE ---\n";
+                            if (hw) {
+                                txt += JSON.stringify(hw, null, 2) + "\n";
+                            } else {
+                                txt += "Nema hardverskih podataka u bazi.\n";
+                            }
 
-                    txt += "\n=== KRAJ DUMPA ===\n";
+                            txt += "\n--- SOFTWARE ---\n";
+                            if (sw) {
+                                txt += JSON.stringify(sw, null, 2) + "\n";
+                            } else {
+                                txt += "Nema softverskih podataka u bazi.\n";
+                            }
 
-                    res.setHeader('Content-disposition', 'attachment; filename=' + safeName + '_debug.txt');
-                    res.setHeader('Content-type', 'text/plain; charset=utf-8');
-                    res.send(txt);
-                }
+                            txt += "\n=== END OF EXPORT ===\n";
+
+                            res.setHeader('Content-disposition', 'attachment; filename=' + safeName + '_ticket.txt');
+                            res.setHeader('Content-type', 'text/plain; charset=utf-8');
+                            res.send(txt);
+                        }
+                    });
+                });
             });
         } else {
             res.sendStatus(404);
